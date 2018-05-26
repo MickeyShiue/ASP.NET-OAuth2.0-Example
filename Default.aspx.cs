c1cd6003062ffc568409b88a954a9163df72b855
MickeyShiue <e031304@gmail.com>
2018-05-26 12:16:36 +0800

    add facebook scope document link

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -41,6 +41,22 @@ namespace WebApplication6
             string redirect_uri = "http://localhost:56506/Default3.aspx"; // CallBack URL code會掉到那頁
             string scpoe = "email,user_gender,user_birthday";  //可以存取那些範圍 ，多個scope 可以用 , 隔開
 
+            #region Permissions Reference - Facebook Login
+
+            //https://developers.facebook.com/docs/facebook-login/permissions/v3.0 //scope document
+
+            //***************Default Fields*****************
+            //id
+            //first_name
+            //last_name
+            //middle_name
+            //name
+            //name_format
+            //picture
+            //short_name
+            //***********************************************
+            #endregion
+
             string url = string.Format("https://www.facebook.com/v3.0/dialog/oauth?client_id={0}&redirect_uri={1}&scope={2}", client_id, redirect_uri, scpoe);
 
             Response.Redirect(url);
